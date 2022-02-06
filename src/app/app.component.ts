a4c93c05e9f62f7c8885e03256a001e6224c57ef
Renegade Asturias <renegade.asturias@gmail.com>
2022-02-06 01:44:01 +0100

    Proyecto MiniCalculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'miniCalculadora';
+  title = 'Mini Calculadora';
+  numero1:number = 100;
+  numero2:number = 11;
+  resultado:number = 0;
+
+  sumar():void{
+    console.log("sumar! "+this.numero1+" + "+this.numero2+" = "+this.resultado);
+    this.resultado=this.numero1+this.numero2;
+  }
+
+  restar():void{
+    console.log("restar! "+this.numero1+" - "+this.numero2+" = "+this.resultado);
+    this.resultado=this.numero1-this.numero2;
+  }
+
 }
